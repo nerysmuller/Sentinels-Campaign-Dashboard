e70fe5517598e28fe7faf090390c3f3e7964bbb2
Nerys Muller <63008208+nerysmuller@users.noreply.github.com>
2025-11-10 11:04:38 -0500

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,1 +1,19 @@
+const playerPasswords = {
+  'aragorn': 'hash_of_password_1',
+  'gandalf': 'hash_of_password_2',
+  'legolas': 'hash_of_password_3'
+};
 
+function authenticatePlayer(username, password) {
+  const hashedInput = simpleHash(password);
+  if (playerPasswords[username] === hashedInput) {
+    sessionStorage.setItem('currentPlayer', username);
+    loadPlayerData(username);
+    return true;
+  }
+  return false;
+}
+
+function simpleHash(str) {
+  return btoa(str);
+}
